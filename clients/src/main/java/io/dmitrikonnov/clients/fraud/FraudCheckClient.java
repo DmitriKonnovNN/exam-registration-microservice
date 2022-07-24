22ee7552e6995ab1172d8184c50956d4d9b021a2
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2022-07-25 01:56:46 +0400

    added: kubernetes settings

--- clients/src/main/java/io/dmitrikonnov/clients/fraud/FraudCheckClient.java
+++ clients/src/main/java/io/dmitrikonnov/clients/fraud/FraudCheckClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "FRAUDCHECK")
+@FeignClient(name = "FRAUDCHECK", url = "${clients.fraudcheck.url}")
 public interface FraudCheckClient {
     @GetMapping(path = "api/v1/fraud-check/{customerId}")
     FraudCheckResponse checkIfCustomerIsFraudster (@PathVariable("customerId") Long customerId);
--- customer/src/main/java/io/dmitrikonnov/customer/CustomerApplication.java
+++ customer/src/main/java/io/dmitrikonnov/customer/CustomerApplication.java
@@ -2,15 +2,17 @@ package io.dmitrikonnov.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication (scanBasePackages = {"io.dmitrikonnov.amqp","io.dmitrikonnov.customer"})
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "io.dmitrikonnov.clients"
 )
+@PropertySources({@PropertySource("classpath:clients-${spring.profiles.active}.properties")})
 public class CustomerApplication {
 
     public static void main(String[] args) {
